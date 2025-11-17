9dee7dd0ac8bf35a879fa6388b300db539755e0b
techdevopscloud <jaygkek@gmail.com>
2025-11-17 07:18:25 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Project for AchiStar Technologies Test1";
+        return "DevOps Project for AchiStar Technologies Test Actions";
     }
 }
